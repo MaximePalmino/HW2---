81be6bdf65c9d4a34a74c2d12267161c17531449
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-03-16 12:00:29 +0100

    [UPDATE]FramerMotion

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,6 +8,10 @@ const TodoList = ({todos, setTodo}) => {
             {todos.slice(0,3).map((todo) => (
                         <motion.div className="motion__div"
                         key={todo.id}
+                        whileHover={{ scale: 1.1 }}
+                        // drag
+                        // dragConstraints={{ left: 0, right: 300 }}
+                        // dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
                         style={{width:'100%'}}
                         initial={{ x: -50, opacity: 0 }}
                         animate={{ x: 0, opacity: 1 }}
